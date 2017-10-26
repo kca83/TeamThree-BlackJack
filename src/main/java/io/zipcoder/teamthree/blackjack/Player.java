53984c565a368f1185bd88744b7e62f50651c11e
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-10-26 15:14:20 -0400

    Add basic functions and tests to Player class

--- src/main/java/io/zipcoder/teamthree/blackjack/Player.java
+++ src/main/java/io/zipcoder/teamthree/blackjack/Player.java
@@ -1,4 +1,45 @@
 package io.zipcoder.teamthree.blackjack;
 
+import java.util.ArrayList;
+
 public class Player {
+    ArrayList<Card> hand = new ArrayList<Card>(2);
+    String name;
+    Double money = Double.valueOf(500);
+    Integer score = Integer.valueOf(0);
+    boolean aceBonus = false;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void addHand(Card card) {
+        hand.add(card);
+    }
+
+    public boolean hasMoneyToMakeBet(Double amount) {
+        return (amount <= money);
+    }
+
+    public void makeBet(Double amount) {
+        money -= amount;
+    }
+
+    
 }
--- src/test/java/io/zipcoder/teamthree/blackjack/PlayerTest.java
+++ src/test/java/io/zipcoder/teamthree/blackjack/PlayerTest.java
@@ -1,4 +1,88 @@
 package io.zipcoder.teamthree.blackjack;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PlayerTest {
+
+    @Test
+    public void getNameTest() {
+        //: Given
+        Player testPlayer = new Player("Gabriela");
+        String expected = "Gabriela";
+
+        //: When
+        String actual = testPlayer.getName();
+
+        //: Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMoneyTest() throws Exception {
+        Player testPlayer = new Player("Claude");
+        Double expected = Double.valueOf(500);
+
+        Double actual = testPlayer.getMoney();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getScoreTest() throws Exception {
+        Player testPlayer = new Player("Zan");
+        Integer expected = Integer.valueOf(0);
+
+        Integer actual = testPlayer.getScore();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getHandTest() throws Exception {
+        Player testPlayer = new Player ("Aaron");
+        testPlayer.addHand(new Card());
+        testPlayer.addHand(new Card());
+        int expected = 2;
+
+        int actual = testPlayer.getHand().size();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void addCardTest() throws Exception {
+        Player testPlayer = new Player ("Pavel");
+        Card card = new Card();
+        testPlayer.addHand(card);
+        Card expected = card;
+
+        Card actual = testPlayer.getHand().get(0);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void hasMoneyToMakeBetTest() {
+        Player testPlayer = new Player("Leon");
+        boolean expected = true;
+
+        boolean actual = testPlayer.hasMoneyToMakeBet(Double.valueOf(100));
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void makeBetTest() {
+        Player testPlayer = new Player("Tim");
+        Double expected = Double.valueOf(400);
+
+        testPlayer.makeBet(Double.valueOf(100));
+        Double actual = testPlayer.getMoney();
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
