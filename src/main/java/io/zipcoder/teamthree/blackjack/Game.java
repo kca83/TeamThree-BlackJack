5488073e3f7d66e0284927b8edbfd468821829ee
senorcl0ud <claudemcalpin@gmail.com>
2017-10-26 17:17:48 -0400

    committing be4 pulling

--- src/main/java/io/zipcoder/teamthree/blackjack/Game.java
+++ src/main/java/io/zipcoder/teamthree/blackjack/Game.java
@@ -1,4 +1,85 @@
 package io.zipcoder.teamthree.blackjack;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class Game {
+    private Player player;
+    private Deck deck;
+    private Player dealer;
+    private Double pot;
+
+    public Game (){
+        player = new Player();
+        deck = new Deck();
+        dealer = new Player();
+        deck.shuffle();
+        pot = player.bet(50.0);
+    }
+
+    public Double getPot() {
+        return pot;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Player getDealer() {
+        return dealer;
+    }
+
+    public void start() {
+
+        //Deal two cards to the player and the dealer
+        for (int i = 0; i < 2; i++)
+        {
+            dealCard(player);
+            dealCard(dealer);
+        }
+
+    }
+
+    public boolean playerWins() {
+        // if both player and dealer scores are = to 21 dealer wins
+        // if both player and dealer score are over 21 then dealer wins
+        // if player's score is <= 21 and players score is greater than dealers score then player wins.
+        //if dealer's score is > 21 and player's score is <= 21 then players wins.
+
+        if((player.getScore().equals(21) && !dealer.getScore().equals(21)) ||
+                (player.getScore()<21 && dealer.getScore() < player.getScore()) ||
+                (player.getScore() < 21 && dealer.getScore() > 21)) {
+            //Player wins
+            return true;
+        }
+        //Dealer wins
+        return false;
+
+
+    }
+
+    public void dealCard(Player playerToReceiveCard){
+        Card card = deck.getCard();
+        playerToReceiveCard.addCardToHand(card);
+    }
+
+    public void askPlayerForHit(Player player) {
+        if (player.getHit() == true) {
+            dealCard(player);
+        }
+    }
+
+//    public void askDealerForHit() {
+//        while (dealer.getScore() <= 17)
+//        {
+//            dealCard(dealer);
+//        }
+//    }
+
+    public void returnBet() {
+        if (playerWins()) {
+            player.addMoney(pot * 2);
+        }
+    }
 }
--- src/test/java/io/zipcoder/teamthree/blackjack/GameTest.java
+++ src/test/java/io/zipcoder/teamthree/blackjack/GameTest.java
@@ -1,4 +1,113 @@
 package io.zipcoder.teamthree.blackjack;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class GameTest {
+
+    @Test
+    public void getPot() {
+        Game game = new Game();
+        Double expectedPot = 50.0;
+        Double actualPot = game.getPot();
+        Assert.assertEquals(expectedPot, actualPot);
+    }
+
+    @Test
+    public void startTest() {
+        Game game = new Game();
+        game.start();
+        int expectedNumberOfPlayerCards = 2;
+        int expectedNumberOfDealerCards = 2;
+
+        Player player = game.getPlayer();
+        Player dealer = game.getDealer();
+
+
+        int actualNumberOfPlayerCards = player.getHandSize();
+        int actualNumberOfDealerCards = dealer.getHandSize();
+
+        Assert.assertEquals(expectedNumberOfPlayerCards, actualNumberOfPlayerCards);
+        Assert.assertEquals(expectedNumberOfDealerCards, actualNumberOfDealerCards);
+    }
+
+    @Test
+    public void playerWinsTest() {
+        Game game = new Game();
+
+        Player player = game.getPlayer();
+        Player dealer = game.getDealer();
+
+        boolean expected = true;
+        boolean expected2 = false;
+        boolean expected3 = true;
+
+        player.setScore();
+        player.getScore();
+        dealer.setScore();
+        dealer.getScore();
+
+        boolean actual = game.playerWins();
+
+        player.setScore();
+        dealer.setScore();
+        player.getScore();
+        dealer.getScore();
+
+        boolean actual2 = game.playerWins();
+
+        player.setScore();
+        dealer.setScore();
+        player.getScore();
+        dealer.getScore();
+
+        boolean actual3 = game.playerWins();
+
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+        Assert.assertEquals(expected3, actual3);
+    }
+
+    @Test
+    public void dealCardTest() {
+        //Calls on methods in other Class Deck and Player.
+    }
+
+    @Test
+    public void askPlayerForHitTest() {
+        Game game = new Game();
+        Player player = game.getPlayer();
+        int expectedHandSize = 1;
+        game.askPlayerForHit(player);
+        int actualHandSize = player.getHandSize();
+        Assert.assertEquals(expectedHandSize, actualHandSize);
+
+    }
+
+    @Test
+    public void askDealerForHit() {
+//        Game game = new Game();
+//        Player dealer = game.getDealer();
+//        int expectedHandSize = 1;
+//        dealer.setScore(16);
+//        game.askDealerForHit();
+//        int actualHandSize = dealer.getHandSize();
+//
+//        int expectedHandSize2 = 0;
+//        dealer.setScore(17);
+//        game.askDealerForHit();
+//        int actualHandSize2 = dealer.getHandSize();
+//
+//        Assert.assertEquals(expectedHandSize, actualHandSize);
+//        Assert.assertEquals(expectedHandSize2, actualHandSize2);
+
+    }
+
+    @Test
+    public void returnBet() {
+        Game game = new Game();
+        Double expected = 50.0;
+        Double actual = game.getPot();
+        Assert.assertEquals(expected, actual, .001);
+    }
 }
