2909ad7f22c8726fd5ef22cc67d00b6609bcc6d8
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-10-26 13:36:45 -0400

    Create tests and code for Card class and Suit enum

--- src/main/java/io/zipcoder/teamthree/blackjack/Card.java
+++ src/main/java/io/zipcoder/teamthree/blackjack/Card.java
@@ -1,4 +1,23 @@
 package io.zipcoder.teamthree.blackjack;
 
-public class Card {
+public class Card
+{
+    private Suit suit;
+    private Integer value;
+
+    private Card(){
+    }
+
+    public Card(Suit passedSuit, Integer passedValue){
+        suit=passedSuit;
+        value=passedValue;
+    }
+
+    public Integer getValue(){
+        return value;
+    }
+
+    public Suit getSuit(){
+        return suit;
+    }
 }
